e06addc3f8ad32619257a2f54c7f9b19cfdf22b8
cympfh <cympfh@gmail.com>
2022-03-29 20:04:41 +0900

    Update easy-scraper-py

--- web_grepy/cli.py
+++ web_grepy/cli.py
@@ -2,38 +2,10 @@
 from __future__ import annotations
 
 import json
-import re
 import sys
-from dataclasses import dataclass
 
 import click
 import easy_scraper
-
-
-@dataclass
-class Pattern:
-    format: str
-
-    @classmethod
-    def build(cls, raw_format: str) -> Pattern:
-        """Build pattern for easy_scraper
-
-        Examples
-        --------
-        >>> build("<img src={} />")
-        Pattern("<img src={{}} />")
-        >>> build("<a href={x}>{y}</a>")
-        Pattern("<a href={{x}}>{{y}}</a>")
-        """
-        format = raw_format
-
-        annoyholder = re.compile(r"{\s*}")
-        format = annoyholder.sub(r"{{}}", format)
-
-        namedholder = re.compile(r"{\s*(\w+)\s*}")
-        format = namedholder.sub(r"{{\1}}", format)
-
-        return Pattern(format)
 
 
 def writer(item: dict, format: str) -> str:
@@ -58,8 +30,7 @@ def main(delimiter: str, format: str, pattern: str):
     Write into stdout.
     """
     html = "\n".join(sys.stdin)
-    p = Pattern.build(pattern)
-    res = easy_scraper.match(html, p.format)
+    res = easy_scraper.match(html, pattern)
     for item in res:
         print(writer(item, format))
 
