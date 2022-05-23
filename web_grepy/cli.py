370e8e8325b93876b0ad9978ce24c2d8b79a4fe2
cympfh <cympfh@gmail.com>
2022-05-24 00:56:56 +0900

    fix delimiter

--- web_grepy/cli.py
+++ web_grepy/cli.py
@@ -8,9 +8,9 @@ import click
 import easy_scraper
 
 
-def writer(item: dict, format: str) -> str:
+def writer(item: dict, format: str, delimiter: str) -> str:
     if format == "ltsv":
-        return "\t".join(f"{k}:{v}" if k else v for (k, v) in item.items())
+        return delimiter.join(f"{k}:{v}" if k else v for (k, v) in item.items())
 
     elif format == "json":
         return json.dumps(item)
@@ -32,7 +32,7 @@ def main(delimiter: str, format: str, pattern: str):
     html = "\n".join(sys.stdin)
     res = easy_scraper.match(html, pattern)
     for item in res:
-        print(writer(item, format))
+        print(writer(item, format, delimiter))
 
 
 if __name__ == "__main__":
